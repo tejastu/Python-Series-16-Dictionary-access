2df69da2f160b1104b3620f67bda23b5e8c6c072
Tejas Parab <62276215+tejastu@users.noreply.github.com>
2021-02-14 20:05:12 +0530

    Add files via upload

---
+++ 16_dict.py
@@ -0,0 +1,12 @@
+myDict = {
+    "Coding":"Programmig beneficial",
+    "Tejas":"A hero",
+    "Marks":[34,21,22],
+    "Anotherone": {
+        "myson": {
+            'daughter':'seema'
+        }
+    }
+}
+print(myDict['Tejas'])
+print(myDict['Anotherone']['myson']['daughter'])
